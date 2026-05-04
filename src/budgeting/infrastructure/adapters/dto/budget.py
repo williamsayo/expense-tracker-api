fbd35d68fdabab5d45330beb3a9778d891ee0540
williams ayo <williamsayo44@gmail.com>
2026-05-04 20:36:50 +0300

    feat: enhance budget and budget allocation models with additional fields and validation

--- src/budgeting/infrastructure/adapters/dto/budget.py
+++ src/budgeting/infrastructure/adapters/dto/budget.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
-from typing import Self, List
+from uuid import UUID
+from typing import Annotated, Self, List
 from datetime import date, timedelta
 from shared.infrastructure.adapters.dto.base import BaseReadModel
 from budgeting.domain.entities.budget_entity import BudgetEntity
@@ -8,15 +9,42 @@ from budgeting.infrastructure.adapters.dto.budget_allocation import (
     BudgetAllocationWriteModel,
     BudgetAllocationUpdateModel,
 )
+from shared.domain.types.currency_types import Currency
+
+Percentage = Annotated[
+    float,
+    Field(
+        ge=0,
+        le=1,
+        strict=True,
+        description="A percentage value between 0 and 1 inclusive",
+    ),
+]
 
 
 class BudgetModel(BaseModel):
     """Data model for budget."""
 
+    name: str | None = Field(None, description="Name of the budget")
+
 
 class BudgetReadModel(BudgetModel, BaseReadModel):
     """Read model for Budget data."""
 
+    budget_id: str | UUID = Field(
+        ...,
+        description="The unique identifier of the budget",
+        serialization_alias="id",
+    )
+    name: str | None = Field(..., description="Name of the budget")
+    currency: Currency = Field(..., description="Currency code (e.g., 'USD', 'EUR')")
+    total_amount: float = Field(..., description="Total budget amount")
+    amount_spent: float = Field(..., description="Total amount spent")
+    remaining_amount: float = Field(..., description="Remaining budget amount")
+    used_percentage: Percentage = Field(..., description="Percentage of budget used")
+    remaining_percentage: Percentage = Field(
+        default=100, description="Remaining budget percentage"
+    )
     start_date: date = Field(..., description="Date the Budget was made")
     end_date: date = Field(..., description="Date the Budget ends")
     allocations: List[BudgetAllocationReadModel] = Field(
@@ -26,18 +54,44 @@ class BudgetReadModel(BudgetModel, BaseReadModel):
     @classmethod
     def from_entity(cls, entity: BudgetEntity) -> Self:
         return cls(
+            budget_id=entity.id.value,
+            name=entity.name,
+            currency=entity.currency,
             start_date=entity.budget_period.start_date,
             end_date=entity.budget_period.end_date,
             allocations=[
                 BudgetAllocationReadModel.from_entity(allocation)
                 for allocation in entity.allocations
             ],
+            total_amount=0,
+            amount_spent=0,
+            remaining_amount=0,
+            used_percentage=0,
+            remaining_percentage=0,
         )
+
+
+class BudgetOverviewReadModel(BaseReadModel):
+    """Read model for Budget data."""
+
+    total_allocated: float = Field(..., description="Total budget amount")
+    active_budget: BudgetReadModel | None = Field(
+        ..., description="Active budget for the user"
+    )
+    recent_budgets: List[BudgetReadModel] = Field(
+        ..., description="List of recent budgets for the user"
+    )
+    upcoming_budget: BudgetReadModel | None = Field(
+        ..., description="upcoming budgets for the user"
+    )
 
 
 class BudgetWriteModel(BudgetModel):
     """Write model for Budget data."""
 
+    currency: Currency = Field(
+        default=Currency.EUR, description="Currency code (e.g., 'USD', 'EUR')"
+    )
     start_date: date = Field(
         default=date.today(), description="Date the Budget was made"
     )
@@ -51,6 +105,10 @@ class BudgetWriteModel(BudgetModel):
 
 class BudgetUpdateModel(BudgetModel):
     """Update model for Budget data."""
+
+    currency: Currency | None = Field(
+        default=Currency.EUR, description="Currency code (e.g., 'USD', 'EUR')"
+    )
     start_date: date | None = Field(None, description="Date the Budget was made")
     end_date: date | None = Field(None, description="Date the Budget ends")
     allocation: BudgetAllocationUpdateModel | None = Field(
--- src/budgeting/infrastructure/adapters/dto/budget_allocation.py
+++ src/budgeting/infrastructure/adapters/dto/budget_allocation.py
@@ -1,44 +1,67 @@
+from uuid import UUID
 from pydantic import BaseModel, Field
-from typing import Self
+from typing import Annotated, Self
 from decimal import Decimal
 from shared.infrastructure.adapters.dto.base import BaseReadModel
 from budgeting.domain.entities.budget_allocation_entity import BudgetAllocationEntity
 from shared.domain.types.category_types import CategoryType
 from shared.domain.types.currency_types import Currency
 
+Percentage = Annotated[
+    float,
+    Field(
+        ge=0,
+        le=1,
+        strict=True,
+        description="A percentage value between 0 and 1 inclusive",
+    ),
+]
 
 class BudgetAllocationModel(BaseModel):
     """Data model for budget allocation."""
 
     amount: Decimal = Field(
-        ..., description="The amount allocated", ge=Decimal("0.50"), decimal_places=2
+        ...,
+        description="The amount allocated",
+        ge=Decimal("0.50"),
+        decimal_places=2,
+        examples=["100.00", "250.50"],
     )
     category: CategoryType = Field(..., description="The category of the allocation")
 
 
-class BudgetAllocationReadModel(BudgetAllocationModel, BaseReadModel):
+class BudgetAllocationReadModel(BaseReadModel):
     """Read model for BudgetAllocation data."""
 
-    amount: str = Field(
+    allocation_id: str | UUID = Field(
         ...,
-        description="The amount allocated as a formatted currency string to preserve precision",
+        description="The unique identifier of the budget allocation",
+        serialization_alias="id",
+    )
+    spent_amount: float = Field(
+        ...,
+        description="The amount spent from this allocation",
+        serialization_alias="amountSpent",
+    )
+    used_percentage: Percentage = Field(..., description="Percentage of budget used")
+    category: CategoryType = Field(..., description="The category of the allocation")
+    budget_amount: float = Field(
+        ..., description="The amount allocated", serialization_alias="allocatedAmount"
     )
 
     @classmethod
     def from_entity(cls, entity: BudgetAllocationEntity) -> Self:
         return cls(
-            amount=entity.money.to_currency(),
+            allocation_id=entity.id.value,
+            spent_amount=0,  # Placeholder for spent amount, to be calculated based on expenses
+            budget_amount=entity.amount.value,
             category=entity.category.name,
+            used_percentage=0.0,  # Placeholder for used percentage, to be calculated based on expenses
         )
-
 
 class BudgetAllocationWriteModel(BudgetAllocationModel):
     """Write model for BudgetAllocation data."""
-
-    currency: Currency = Field(
-        default=Currency.EUR, description="The currency of the allocation"
-    )
-
+    ...
 
 class BudgetAllocationUpdateModel(BudgetAllocationModel):
     """Update model for BudgetAllocation data."""
@@ -51,7 +74,4 @@ class BudgetAllocationUpdateModel(BudgetAllocationModel):
         decimal_places=2,
         max_digits=12,
         examples=["100.00", "250.50"],
-    )
-    currency: Currency | None = Field(
-        None, description="The currency of the allocation"
     )

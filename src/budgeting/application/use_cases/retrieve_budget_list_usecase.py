65629c420e9b0c837101f9b5a1c820c816519059
williams ayo <williamsayo44@gmail.com>
2026-05-04 20:34:32 +0300

    feat: implement use cases for budget management including add, create, delete, remove allocation, retrieve, and update

---
+++ src/budgeting/application/use_cases/retrieve_budget_list_usecase.py
@@ -0,0 +1,27 @@
+from typing import Protocol, Sequence
+from boilerplate import CoreError
+from result import Either, is_fail, result_ok, result_fail
+from shared.domain.types.user_id import UserId
+from boilerplate.errors.repository import (
+    RepositoryUnexpectedError,
+    DataIntegrityError,
+)
+from budgeting.domain.read_models.budget_summary import BudgetSummaryReadModel
+from budgeting.utils.setup_dependencies import BudgetReadDeps
+
+
+class GetBudgetsUsecase:
+    def __init__(self, deps: BudgetReadDeps):
+        self.deps = deps
+
+    async def execute(self, user_id: UserId) -> Either[
+        Sequence[BudgetSummaryReadModel],
+        CoreError | RepositoryUnexpectedError | DataIntegrityError,
+    ]:
+        """Lists all budgets."""
+        result = await self.deps.repo.list({"filter": {"user_id": user_id}})
+
+        if is_fail(result):
+            return result
+
+        return result_ok(result.value)

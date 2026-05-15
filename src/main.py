3f09900f0e4550a6d0a3352ff147b4e357956bbe
williams ayo <williamsayo44@gmail.com>
2026-05-15 08:59:32 +0300

    refactor: replace database initialization with DynamoDB setup and clean up unused imports

--- src/main.py
+++ src/main.py
@@ -1,30 +1,20 @@
 from fastapi import FastAPI, APIRouter
 from src.core.config import settings
 from src.shared.loggers.logging import setup_logging, LogLevel
-from src.shared.infrastructure.db.base import engine, AsyncSessionLocal
-from src.shared.infrastructure.db.dependencies import init_db
-from src.shared.infrastructure.dispatcher.dependencies import register_handler
-from src.shared.domain.types.event_types import EventTypes
+from src.shared.infrastructure.db.dependencies import init_dynamodb
 from src.core.exception_handler import register_errors
 from src.core.middlewares import register_middlewares
 from contextlib import asynccontextmanager
 from src.identity.presentation.web.v1.route import router as identity_router
 from src.spending.expenses.presentation.web.v1.route import router as expense_router
 from src.spending.budgeting.presentation.web.v1.route import router as budget_router
-from src.spending.budgeting.application.services.event_handler import (
-    OnExpenseCreated,
-    OnBudgetCreated,
-)
 
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
-    await init_db()
     setup_logging(LogLevel.INFO)
-    # register_handler(EventTypes.EXPENSE_CREATED, OnExpenseCreated(AsyncSessionLocal))
-    # register_handler(EventTypes.BUDGET_CREATED, OnBudgetCreated(AsyncSessionLocal))
+    await init_dynamodb()
     yield
-    await engine.dispose()
 
 
 app = FastAPI(
@@ -39,10 +29,12 @@ router = APIRouter()
 register_errors(app=app)
 register_middlewares(app=app)
 
+
 # health check endpoint
 @router.get("/healthz", tags=["Health Check"])
 async def health_check():
     return {"status": "ok"}
+
 
 app.include_router(router)
 app.include_router(identity_router, prefix="/api/v1")

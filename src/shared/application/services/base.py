17e68a0f545bd3f3bec1064d48597076cbe9595f
williams ayo <williamsayo44@gmail.com>
2026-05-19 08:10:07 +0300

    refactor: remove default dependency in BaseService constructor for clarity

--- src/shared/application/services/base.py
+++ src/shared/application/services/base.py
@@ -1,8 +1,19 @@
-from fastapi import Depends
-
-
 class BaseService[T]:
     """Coordinates base application workflows."""
 
-    def __init__(self, deps: T = Depends()):
+    def __init__(self, deps: T):
         self.deps = deps
+
+
+class CommandService[T]:
+    """Base class for command services."""
+
+    def __init__(self, commandDeps: T):
+        self.deps = commandDeps
+
+
+class QueryService[T]:
+    """Base class for query services."""
+
+    def __init__(self, queryDeps: T):
+        self.deps = queryDeps

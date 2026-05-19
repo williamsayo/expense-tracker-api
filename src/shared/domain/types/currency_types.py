da336d8535ed3d5c2e6256d64097d42a3bc1a886
williams ayo <williamsayo44@gmail.com>
2026-05-19 08:16:29 +0300

    refactor: rename currency_display to currency_symbols for clarity

--- src/shared/domain/types/currency_types.py
+++ src/shared/domain/types/currency_types.py
@@ -11,10 +11,18 @@ class Currency(StrEnum):
     GBP = "GBP"
 
 
-currency_display: dict[Currency, str] = {
+currency_symbols: dict[Currency, str] = {
     Currency.EUR: "€",
     Currency.GHS: "GH₵",
     Currency.NGN: "₦",
     Currency.USD: "$",
     Currency.GBP: "£",
+}
+
+CURRENCY_FACTORS = {
+    Currency.EUR: 100,
+    Currency.GHS: 100,
+    Currency.NGN: 100,
+    Currency.USD: 100,
+    Currency.GBP: 100,
 }

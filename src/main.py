e0d4c20d31c9e107bab37258a1cdde46c2e8f526
williams ayo <williamsayo44@gmail.com>
2026-05-20 23:58:43 +0300

    refactor: remove AWS S3 bucket initialization from lifespan function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI, APIRouter
 from src.core.config import get_settings
-from src.shared.infrastructure.services.aws.config import get_aioboto3_session
-from src.shared.infrastructure.services.aws.dependencies import get_s3_client
-from src.shared.infrastructure.services.aws.utils import (
-    create_bucket,
-    set_bucket_policy,
-    set_public_access,
-    PUBLIC_READ_POLICY,
-)
 from src.shared.loggers.logging import setup_logging, LogLevel
 from src.shared.infrastructure.db.base import engine, AsyncSessionLocal
 from src.shared.infrastructure.db.dependencies import init_db
@@ -32,14 +24,6 @@ settings = get_settings()
 async def lifespan(_app: FastAPI):
     await init_db()
     setup_logging(LogLevel.INFO)
-    # Initialize AWS S3 bucket
-    async with get_aioboto3_session().client("s3") as s3_client:
-        await create_bucket(settings.aws_s3_bucket_name, s3_client)
-        # await set_public_access(s3_client, settings.aws_s3_bucket_name)
-        # await set_bucket_policy(
-        #    s3_client, settings.aws_s3_bucket_name, PUBLIC_READ_POLICY
-        # )
-
     # register_handler(EventTypes.EXPENSE_CREATED, OnExpenseCreated(AsyncSessionLocal))
     # register_handler(EventTypes.BUDGET_CREATED, OnBudgetCreated(AsyncSessionLocal))
     yield

0bc1c680b3201e17bb35415675ba97b6ff25c9d5
williams ayo <williamsayo44@gmail.com>
2026-05-20 23:55:54 +0300

    refactor: update upload_avatar and upload_receipt methods to include content_type, username, and user_id parameters

--- src/shared/infrastructure/adapters/ports/repository.py
+++ src/shared/infrastructure/adapters/ports/repository.py
@@ -5,9 +5,21 @@ from result import Either
 
 class ObjectStorageRepository(Protocol):
     async def upload_avatar(
-        self, key: str, file: BinaryIO
+        self,
+        key: str,
+        file: BinaryIO,
+        *,
+        content_type: str = "image/png",
+        username: str | None = None,
+        user_id: str | None = None,
     ) -> Either[str, UnexpectedError]: ...
 
     async def upload_receipt(
-        self, key: str, file: BinaryIO
+        self,
+        key: str,
+        file: BinaryIO,
+        *,
+        content_type: str = "application/pdf",
+        username: str | None = None,
+        user_id: str | None = None,
     ) -> Either[str, UnexpectedError]: ...
--- src/shared/infrastructure/repository/s3/s3_repo.py
+++ src/shared/infrastructure/repository/s3/s3_repo.py
@@ -1,14 +1,13 @@
 from enum import StrEnum
 from typing import BinaryIO
 from types_aiobotocore_s3 import S3Client
-from result import result_fail, result_ok, Either
+from result import is_fail, result_fail, result_ok, Either
 from boilerplate import UnexpectedError
 from src.shared.infrastructure.services.aws.utils import (
     create_bucket,
-    generate_object_url,
+    generate_bucket_url,
 )
 from src.core.config import Settings
-
 
 class FolderPath(StrEnum):
     receipts = "receipts"
@@ -26,33 +25,70 @@ class S3BucketRepository:
         await create_bucket(bucket_name, self.client)
 
     async def upload_avatar(
-        self, key: str, file: BinaryIO
+        self,
+        key: str,
+        file: BinaryIO,
+        *,
+        content_type: str = "image/png",
+        username: str | None = None,
+        user_id: str | None = None,
     ) -> Either[str, UnexpectedError]:
         key = self.construct_key(key, FolderPath.avatars)
-        await self.upload_file_object(key=key, data=file)
-        return result_ok(generate_object_url(key))
+
+        result = await self.upload_file_object(
+            key, file, content_type=content_type, username=username, user_id=user_id
+        )
+
+        if is_fail(result):
+            return result_fail(result.value)
+        return result_ok(key)
 
     async def upload_receipt(
-        self, key: str, file: BinaryIO
+        self,
+        key: str,
+        file: BinaryIO,
+        *,
+        content_type: str = "application/pdf",
+        username: str | None = None,
+        user_id: str | None = None,
     ) -> Either[str, UnexpectedError]:
         key = self.construct_key(key, FolderPath.receipts)
-        await self.upload_file_object(key=key, data=file)
-        return result_ok(generate_object_url(key))
+
+        result = await self.upload_file_object(
+            key, file, content_type=content_type, username=username, user_id=user_id
+        )
+
+        if is_fail(result):
+            return result_fail(result.value)
+
+        return result_ok(key)
 
     async def upload_file_object(
         self,
         key: str,
         data: BinaryIO,
+        *,
+        content_type: str,
+        username: str | None = None,
+        user_id: str | None = None,
     ) -> Either[str, UnexpectedError]:
+
+        uploaded_by_metadata = {"uploaded-by": username} if username else {}
+        user_id_metadata = {"user-id": user_id} if user_id else {}
+
         try:
             await self.client.upload_fileobj(
                 Bucket=self.bucket_name,
                 Key=key,
                 Fileobj=data,
+                ExtraArgs={
+                    "ContentType": content_type,
+                    "Metadata": {**uploaded_by_metadata, **user_id_metadata},
+                },
             )
-            return result_ok(generate_object_url(key))
+            return result_ok(self.generate_object_url(key))
         except Exception as error:
-            return result_fail(UnexpectedError(error))
+            return result_fail(UnexpectedError(error, "Failed to upload file to S3"))
 
     async def put_object(
         self, key: str, data: bytes, *, location: FolderPath
@@ -64,24 +100,26 @@ class S3BucketRepository:
                 Body=data,
             )
 
-            return result_ok(generate_object_url(key))
+            return result_ok(self.generate_object_url(key))
 
         except Exception as error:
-            return result_fail(UnexpectedError(error))
+            return result_fail(UnexpectedError(error, "Failed to upload file to S3"))
 
     async def delete_object(self, key: str) -> Either[None, UnexpectedError]:
         try:
             await self.client.delete_object(Bucket=self.bucket_name, Key=key)
             return result_ok(None)
         except Exception as error:
-            return result_fail(UnexpectedError(error))
+            return result_fail(
+                UnexpectedError(error, "Failed to delete object from S3")
+            )
 
     async def get_object(self, key: str) -> Either[bytes, UnexpectedError]:
         try:
             response = await self.client.get_object(Bucket=self.bucket_name, Key=key)
             return result_ok(await response["Body"].read())
         except Exception as error:
-            return result_fail(UnexpectedError(error))
+            return result_fail(UnexpectedError(error, "Failed to get object from S3"))
 
     async def presigned_url(self, key: str) -> Either[str, UnexpectedError]:
         try:
@@ -96,3 +134,6 @@ class S3BucketRepository:
 
     def construct_key(self, filename: str, folder: FolderPath) -> str:
         return f"{folder}/{filename}"
+
+    def generate_object_url(self, key: str) -> str:
+        return generate_bucket_url(key)

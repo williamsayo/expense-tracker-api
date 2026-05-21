7e26e0f243aabc1da7e59c47a316d188bf2dee4e
williams ayo <williamsayo44@gmail.com>
2026-05-21 12:17:07 +0300

    refactor: update error handling to use HttpError for file validation

--- src/shared/application/services/image_validation.py
+++ src/shared/application/services/image_validation.py
@@ -1,26 +1,13 @@
 from typing import Text
 import uuid
 from fastapi import UploadFile
-from boilerplate import (
-    CoreError,
-    ApplicationError,
-    BadRequestError,
-    BadGatewayError,
-    UnexpectedError,
-)
+from boilerplate import BadRequestError, HttpError, HttpStatus
 from result import result_combine, result_ok, result_fail, is_fail, Either
 from pathlib import Path
 import magic
-
+from src.shared.errors.error_ids import ERROR_IDS
 from src.shared.application.dtos.upload import FileUploadDTO
 
-MAGIC_BYTES = {
-    b"\xff\xd8\xff": "image/jpeg",
-    b"\x89PNG\r\n": "image/png",
-    b"RIFF": "image/webp",  # also check bytes 8-12 == b"WEBP"
-    b"GIF87a": "image/gif",
-    b"GIF89a": "image/gif",
-}
 ALLOWED_CONTENT_TYPES = {"image/jpeg", "image/png", "image/webp", "image/gif"}
 ALLOWED_EXTENSIONS = {".jpg", ".jpeg", ".png", ".webp", ".gif"}
 MAX_SIZE_BYTES = 5 * 1024 * 1024  # 5 MB
@@ -30,7 +17,7 @@ class ImageValidationService:
     def __init__(self, file: UploadFile):
         self.file = file
 
-    async def validate_file(self) -> Either[FileUploadDTO, BadRequestError]:
+    async def validate_file(self) -> Either[FileUploadDTO, BadRequestError | HttpError]:
         file_size_result = await self.validate_file_size()
         declared_content_type = self.validate_content_type()
         content_type = await self.validate_magic_bytes(
@@ -63,7 +50,7 @@ class ImageValidationService:
 
     async def validate_magic_bytes(
         self, declared_content_type: str | None
-    ) -> Either[Text, BadRequestError]:
+    ) -> Either[Text, HttpError]:
         """Validate the file's magic bytes to confirm its content type.
         Checks performed:
         - Read the first 512 bytes of the file
@@ -73,7 +60,7 @@ class ImageValidationService:
         Args:
             declared_content_type (str | None): The content type declared by the client, if any.
             Returns:
-            Either[Text, BadRequestError]: Ok with detected MIME type or Fail with error.
+            Either[Text, HttpError]: Ok with detected MIME type or Fail with error.
         """
         header = await self.file.read(512)
         await self.file.seek(0)
@@ -81,24 +68,36 @@ class ImageValidationService:
         mime = magic.from_buffer(header, mime=True)
         if mime not in ALLOWED_CONTENT_TYPES:
             return result_fail(
-                BadRequestError("File content does not match declared type")
+                HttpError(
+                    status=HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+                    body={
+                        "id": ERROR_IDS.INVALID_CONTENT_TYPE, 
+                        "message": "Mime type not allowed ",
+                    },
+                )
             )
 
         if declared_content_type and mime != declared_content_type:
             return result_fail(
-                BadRequestError("File content does not match declared type")
+                HttpError(
+                    status=HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+                    body={
+                        "id": ERROR_IDS.INVALID_CONTENT_TYPE,
+                        "message": "File content does not match Content-Type declaration",
+                    },
+                )
             )
 
         return result_ok(mime)
 
-    def validate_content_type(self) -> Either[str | None, BadRequestError]:
+    def validate_content_type(self) -> Either[str | None, HttpError]:
         """Validate the declared content type of the uploaded file.
         Checks performed:
         - Presence of content type
         - Allowed content types
 
         Returns:
-            Either[str | None, BadRequestError]: Ok with content type or Fail with error.
+            Either[str | None, HttpError]: Ok with content type or Fail with error.
         """
         content_type = (
             self.file.content_type.strip().lower() if self.file.content_type else None
@@ -106,7 +105,13 @@ class ImageValidationService:
 
         if content_type and content_type not in ALLOWED_CONTENT_TYPES:
             return result_fail(
-                BadRequestError(f"Content type not allowed: {content_type}")
+                HttpError(
+                    status=HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+                    body={
+                        "id": ERROR_IDS.INVALID_CONTENT_TYPE,
+                        "message": f"Content type not allowed: {content_type}",
+                    },
+                )
             )
 
         return result_ok(content_type)
@@ -131,28 +136,36 @@ class ImageValidationService:
 
             # Null bytes
             if "\x00" in name:
-                return result_fail(BadRequestError("Invalid filename"))
+                return result_fail(
+                    BadRequestError(ERROR_IDS.INVALID_FILENAME, "Invalid filename")
+                )
 
             suffixes = Path(name).suffixes
 
             for ext in suffixes:
                 if ext.lower() not in ALLOWED_EXTENSIONS:
                     return result_fail(
-                        BadRequestError(f"Disallowed file extension: {ext}")
+                        BadRequestError(
+                            ERROR_IDS.DISALLOWED_FILE_EXTENSION,
+                            f"Disallowed file extension: {ext}",
+                        )
                     )
 
             final_ext = Path(name).suffix.lower()
 
             if final_ext not in ALLOWED_EXTENSIONS:
                 return result_fail(
-                    BadRequestError(f"Extension not allowed: {final_ext}")
+                    BadRequestError(
+                        ERROR_IDS.DISALLOWED_FILE_EXTENSION,
+                        f"Extension not allowed: {final_ext}",
+                    )
                 )
 
             return result_ok((name, final_ext))
 
         return result_ok((filename, ""))
 
-    async def validate_file_size(self) -> Either[int, BadRequestError]:
+    async def validate_file_size(self) -> Either[int, HttpError]:
         """Validate the file size by reading it in chunks.
         This approach avoids loading the entire file into memory, which is important for large files.
          Checks performed:
@@ -160,14 +173,22 @@ class ImageValidationService:
          - Keep a running total of the file size
          - If the total exceeds the maximum allowed size, return an error
          Returns:
-             Either[None, BadRequestError]: Ok if file size is within limits, or Fail with error.
+             Either[None, HttpError]: Ok if file size is within limits, or Fail with error.
         """
         file_size = 0
 
         while chunk := await self.file.read(8192):
             file_size += len(chunk)
             if file_size > MAX_SIZE_BYTES:
-                return result_fail(BadRequestError("File size exceeds limit"))
+                return result_fail(
+                    HttpError(
+                        status=HttpStatus.PAYLOAD_TOO_LARGE,
+                        body={
+                            "id": ERROR_IDS.INVALID_FILE_SIZE,
+                            "message": "File size exceeds limit",
+                        },
+                    )
+                )
 
         await self.file.seek(0)
 

37f5ca157f702590674925840c884bcdf2e5efcc
williams ayo <williamsayo44@gmail.com>
2026-05-23 11:37:06 +0300

    feat: add dynamodb_dashboard_table_name to settings configuration

--- src/core/config.py
+++ src/core/config.py
@@ -44,6 +44,7 @@ class Settings(BaseSettings):
     google_api_key: SecretStr
     cloudfront_private_key: SecretStr
     cloudfront_key_pair_id: str
+    dynamodb_dashboard_table_name: str = "dashboard_table"
 
     model_config = SettingsConfigDict(
         env_file=(".env.local", ".env", ".env.prod"), extra="ignore"

868273fe9163860a8d5f69b722051cdee37fc9c7
williams ayo <williamsayo44@gmail.com>
2026-05-23 11:34:01 +0300

    feat: add DynamoDB client support and improve AWS service dependencies

--- src/shared/infrastructure/services/aws/dependencies.py
+++ src/shared/infrastructure/services/aws/dependencies.py
@@ -1,4 +1,5 @@
 from typing import AsyncGenerator
+from types_aiobotocore_dynamodb import DynamoDBClient, DynamoDBServiceResource
 from src.shared.infrastructure.services.aws.config import get_aioboto3_session
 from types_aiobotocore_cloudfront import CloudFrontClient
 from types_aiobotocore_s3 import S3Client
@@ -8,6 +9,12 @@ async def get_s3_client() -> AsyncGenerator[S3Client, None]:
     async with get_aioboto3_session().client("s3") as s3_client:
         yield s3_client
 
+
 async def get_cloudfront_client() -> AsyncGenerator[CloudFrontClient, None]:
     async with get_aioboto3_session().client("cloudfront") as cloudfront_client:
         yield cloudfront_client
+
+
+async def get_dynamodb_client() -> AsyncGenerator[DynamoDBServiceResource, None]:
+    async with get_aioboto3_session().resource("dynamodb") as dynamodb_client:
+        yield dynamodb_client

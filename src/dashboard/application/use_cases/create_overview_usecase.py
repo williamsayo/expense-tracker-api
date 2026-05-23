362d345e56dc427843a75ae79db32e12c96db918
williams ayo <williamsayo44@gmail.com>
2026-05-23 22:28:19 +0300

    feat: simplify overview creation by removing unused read model and recent expenses handling

--- src/dashboard/application/use_cases/create_overview_usecase.py
+++ src/dashboard/application/use_cases/create_overview_usecase.py
@@ -7,13 +7,9 @@ from boilerplate import (
     AsyncCommandUseCase,
     CoreError,
 )
-from result import Either, is_fail, result_combine, result_fail, result_ok
+from result import Either, is_fail, result_fail, result_ok
 from src.dashboard.utils.setup_dependencies import OverviewDeps
-from src.dashboard.domain.read_models.overview_read_model import (
-    DashboardOverviewReadModel,
-)
-from src.dashboard.infrastructure.adapters.dto.dashboard import DashboardWriteModel
-
+from src.dashboard.infrastructure.adapters.dto.dashboard import DashboardReadModel
 
 class CreateOverviewInput(TypedDict):
     user_id: str
@@ -35,19 +31,13 @@ class CreateOverviewUsecase(AsyncCommandUseCase[CreateOverviewInput]):
 
         user_id, data = input["user_id"], input["overview_data"]
 
-        dashboard_overview = DashboardOverviewReadModel(user_id=user_id, **data)
+        dashboard_overview = DashboardReadModel(user_id=user_id, **data)
 
         result = await self.deps.repository.add(
             dashboard_overview, sort_key=f"overview#{date.today().isoformat()}"
         )
 
-        recent_expenses_result = await self.deps.repository.add_recent_expense(
-            user_id, []
-        )
+        if is_fail(result):
+            return result_fail(result.value)
 
-        combined_result = result_combine((result, recent_expenses_result))
-
-        if is_fail(combined_result):
-            return combined_result
-
-        return result_ok(None)
+        return result_ok(result.value)
